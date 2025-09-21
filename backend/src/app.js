d59a488cc51be463a5ee6c79e5a189f1f7934880
VishalYa6 <vy372984@gmail.com>
2025-09-21 15:42:35 +0530

    auth-route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 //create server
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth.routes');
+
 
 const app = express();
 app.use(cookieParser());
@@ -9,6 +11,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
+
+app.use('/api/auth', authRoutes);
 
 module.exports =app;
  
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -11,7 +11,7 @@ async function registerUser(req, res) {
     }
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = new userModel.create({ fullName, email, password: hashedPassword });
+    const user = await userModel.create({ fullName, email, password: hashedPassword });
     const token = jwt.sign({id:user._id },"ea42b685fedf463ee0977b0970c80f35");
     res.cookie("token", token);
     res.status(201).json({message: "User registered successfully", user:{
